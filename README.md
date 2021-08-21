882fa3ca5322e9e1cc05a284047f95d3d501aaf2
Jolo Cansana <jcansana@gmail.com>
2021-08-21 20:13:17 +0800

    fixed project structure, modified packages

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-To install packages, 
+<h1>Rock Paper Scissors Battle Royale</h1>
+Description goes here.
+</br></br>
+
+<h1>Installation</h1>
+
+**Prerequisites**
+- NodeJS
+
+**Installation Script**
+
+```
 npm install
+```
+**Run Script**
+```
+run script goes here
+```
+
+
+<h1>Project Structure</h1>
+assets : design and image assets
+
+- css : css files
+
+config : config files
+
+models : model files for db
+
+routes : js route files
+
+views : handlebar views
+- layouts : handlebar layouts
+- partials : handlebar partials
--- index.js
+++ index.js
@@ -1,9 +1,55 @@
+// ==============
+// Dependencies
+// ==============
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+// ==============
+// Database
+// ==============
+
+// put db link here
+
+// ==============
+// Handlebars
+// ==============
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  //defaultview: 'main',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/',
+  handlebars: allowInsecurePrototypeAccess(hbs),
+  helpers: 
+  {
+      formatDate: function(datetime) 
+      {
+          if (datetime !== null) {
+              format = "MMMM DD YYYY";
+              return moment(datetime).format(format);
+          }
+          else {
+              return "";
+          }
+      },
+
+      formatPrice: function(num)
+      {
+          return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+      }
+  }
+}))
+
+app.set('view engine', 'hbs');
+
+// ==============
+// Routes
+// ==============
 
 app.get('/', (req, res) => {
   res.send("hello world");
