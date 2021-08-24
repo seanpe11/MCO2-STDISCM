e4127f5eaa97d843d8eb3a208fc5a2818e9a279b
Sean Pe <sean.m.s.pe@gmail.com>
2021-08-24 11:58:06 +0800

    Revert "Revert "fixed port properly this time?\""
    This reverts commit 871ebc8603fca0c1041282f9e54fcd9408753ec2.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = (process.env.PORT || 3000)
 
 // ==============
 // Database
@@ -64,6 +65,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen((3000 || process.env.PORT), () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log('listening on *:' + port);
 });
