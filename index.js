283ab874144cc497c73db15c0ce4b2d30e469a27
Sean Pe <sean.m.s.pe@gmail.com>
2021-08-24 11:35:40 +0800

    added port

--- index.js
+++ index.js
@@ -64,6 +64,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen((3000 || PROCESS.ENV.PORT), () => {
+server.listen((3000 || process.env.PORT), () => {
   console.log('listening on *:3000');
 });
